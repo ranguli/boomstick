049814fe0a20b1a562ad66eb06bc81412769dc6a
ranguli <hello@joshmurphy.ca>
2022-02-06 21:11:25 -0330

    Tweak weapons code structuring

--- api.lua
+++ api.lua
@@ -6,5 +6,3 @@ local modpath = minetest.get_modpath(minetest.get_current_modname())
 dofile(modpath .. "/api/helpers.lua")
 dofile(modpath .. "/api/projectile.lua")
 dofile(modpath .. "/api/weapon.lua")
-dofile(modpath .. "/api/weapon_functions.lua")
-
--- init.lua
+++ init.lua
@@ -5,7 +5,6 @@ local modpath = minetest.get_modpath(minetest.get_current_modname())
 dofile(modpath .. "/api.lua")
 
 dofile(modpath .. "/projectiles.lua")
--- dofile(modpath .. "/weapons.lua")
 
 dofile(modpath .. "/weapons/weapon_categories.lua")
 dofile(modpath .. "/weapons/weapon_definitions.lua")
